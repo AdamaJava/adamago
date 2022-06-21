60ecb4985ed0419760c8b468ecb5a3d9c0a72258
John Pearson <grendeloz@gmail.com>
2022-06-21 14:48:20 +1000

    Change qmotif go code filename and added package.

--- xml/qmotif.go
+++ xml/qmotif.go
@@ -53,57 +53,54 @@
 //   </regions>
 // </qmotif>
 
+package xml
 
 import (
-    "encoding/xml"
+	"encoding/xml"
 )
 
 type Qmotif struct {
-    XMLName xml.Name     `xml:"qmotif"`
-    Version string       `xml:"version,attr"`
-    Ini     Ini          `xml:"ini"`
+	XMLName xml.Name `xml:"qmotif"`
+	Version string   `xml:"version,attr"`
+	Ini     Ini      `xml:"ini"`
 }
 
 type Ini struct {
-    XMLName  xml.Name     `xml:"ini"`
-    File     string       `xml:"file,attr"`
-    S1motif  Stage1_motif `xml:"stage1_motif"`
-    S2motif  Stage2_motif `xml:"stage2_motif"`
-    Includes []Region     `xml:"includes"`
+	XMLName  xml.Name     `xml:"ini"`
+	File     string       `xml:"file,attr"`
+	S1motif  Stage1_motif `xml:"stage1_motif"`
+	S2motif  Stage2_motif `xml:"stage2_motif"`
+	Includes []Region     `xml:"includes"`
 }
 
 type Stage1_motif struct {
-    XMLName xml.Name `xml:"stage1_motif"`
-    Strings []String `xml:"string"`
-    Regexs  []Regex  `xml:"regex"`
+	XMLName xml.Name `xml:"stage1_motif"`
+	Strings []String `xml:"string"`
+	Regexs  []Regex  `xml:"regex"`
 }
 
 type String struct {
-    XMLName xml.Name `xml:"string"`
-    Value   string   `xml:"value,attr"`
+	XMLName xml.Name `xml:"string"`
+	Value   string   `xml:"value,attr"`
 }
 
 type Regex struct {
-    XMLName xml.Name `xml:"regex"`
-    Value   string   `xml:"value,attr"`
+	XMLName xml.Name `xml:"regex"`
+	Value   string   `xml:"value,attr"`
 }
 
 type Window_size struct {
-    XMLName xml.Name `xml:"window_size"`
-    Value   string   `xml:"value,attr"`
+	XMLName xml.Name `xml:"window_size"`
+	Value   string   `xml:"value,attr"`
 }
 
 type Includes_only struct {
-    XMLName xml.Name `xml:"includes_only"`
-    Value   string   `xml:"value,attr"`
+	XMLName xml.Name `xml:"includes_only"`
+	Value   string   `xml:"value,attr"`
 }
 
 type Region struct {
-    XMLName xml.Name `xml:"region"`
-    ChrPos  string   `xml:"chrPos,attr"`
-    Name    string   `xml:"name,attr"`
+	XMLName xml.Name `xml:"region"`
+	ChrPos  string   `xml:"chrPos,attr"`
+	Name    string   `xml:"name,attr"`
 }
-
-
-
-
